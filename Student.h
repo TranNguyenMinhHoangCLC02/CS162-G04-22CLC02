a74aba481ea6873d4ca9e7a635790ee2acfc9aca
TanChau <ntchau22@clc.fitus.edu.vn>
2023-04-09 20:18:08 +0700

    define function at Student.h

--- Student.h
+++ Student.h
@@ -6,4 +6,17 @@
 void student_info(string username);
 void viewScoreBoard_Student(string username);
 
+int numOfStudent(Course* course);
+void addStudentToCourse(Course* &course, Student* &student);
+
+void printCourseInfo(Course* course);
+void viewCoursesInSemester(Student* student, Semester* semester);
+
+void deleteCourse(Course *&course_head, Course* course);
+
+bool isLoggedIn();
+void login(bool isStudent, string &username);
+
+void removeStudentFromCourse(Course* &course, Student* &student) ;
+
 #endif
