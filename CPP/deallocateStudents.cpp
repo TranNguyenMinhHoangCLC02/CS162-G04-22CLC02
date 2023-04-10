680dbe174785f1496b735a00ab48be28741a9582
TNMH12 <tnmhoang.lop93@gmail.com>
2023-04-10 15:12:06 +0700

    add deallocateStudents function

---
+++ CPP/deallocateStudents.cpp
@@ -0,0 +1,18 @@
+#include "../Header/Structures.h"
+
+void dellocateStudents(Student *&head_student)
+{
+    while (head_student != nullptr)
+    {
+        Student *temp_student = head_student;
+        head_student = head_student->student_next;
+        Course *curr_course = temp_student->course_head;
+        while (curr_course != nullptr)
+        {
+            Course *temp_course = curr_course;
+            curr_course = curr_course->course_next;
+            delete temp_course;
+        } 
+        delete temp_student;
+    }
+}
