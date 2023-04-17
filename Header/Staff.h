f273d40bd0a6ae6d2ea149be6ba24f9fed27a753
TanChau <ntchau22@clc.fitus.edu.vn>
2023-04-17 14:23:38 +0700

    fix function addStudentToCourse

--- Header/Staff.h
+++ Header/Staff.h
@@ -50,7 +50,7 @@ void addNewCourse (string username, Year* year_head, Semester* semester_head);
 void viewStudentInCourse(string username, Year* &year_head, Semester* &semester_head, Course* &course_head);
 int numOfStudent(Course* course);
 bool isStudentInCourse(Course* course, Student* student);
-Student* createStudent();
+Student* findStudentInClass(Class* class_head, string studentID);
 void addStudentToCourse(string username, Course* &course, Year* &year_head, Semester* semester_head);
 void printCourseInfo(Course* course);
 
